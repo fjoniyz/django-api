73da559932e5e7ea2344a23302e127da189e3402
fjoniyzeiri <fjoniyzeiri@campus.tu-berlin.de>
2021-09-10 10:16:58 +0200

    ok

---
+++ api_project/posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+# Create your models here.
